2c89a680f16e2e8a1b8bfb4248f3296d85e4c414
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-08-31 18:42:40 -0500

    Ajuste en la peticion

--- src/main/java/com/empresaempleados/empleados/controllers/IndexController.java
+++ src/main/java/com/empresaempleados/empleados/controllers/IndexController.java
@@ -6,12 +6,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.empresaempleados.empleados.models.Empleado;
 
 @Controller
-@RequestMapping("/app")
 public class IndexController {
     
     @GetMapping({"/index", "/home", "/", ""})
